1b646f0134bb48fd885d41f24ed3144a58e4b210
dkrivaa <dannykrivaa@hotmail.com>
2023-08-01 13:58:35 +0300

    licence reader

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
 import cv2
+import numpy as np
+import numpy as pd
+import imutils
+import easyocr
+from PIL import Image
 
-img = cv2.imread('licence.jpeg')
 
-cv2.imshow('test', img)
+org_img = cv2.imread('licence1.png')
+
+# resize image to acceptable size
+h, w, _ = org_img.shape
+size = h*w
+if size < 100000:
+    img = cv2.resize(org_img, (0, 0), fx=2, fy=2)
+else:
+    img = org_img
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+edged = cv2.Canny(bfilter, 30, 200)
+
+keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(keypoints)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+location = None
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 10, True)
+    if len(approx) == 4:
+        location = approx
+        break
+print(location)
+
+mask = np.zeros(gray.shape, np.uint8)
+new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+new_image = cv2.bitwise_and(img, img, mask=mask)
+
+(x,y) = np.where(mask==255)
+(x1, y1) = (np.min(x), np.min(y))
+(x2, y2) = (np.max(x), np.max(y))
+cropped_image = gray[x1:x2+1, y1:y2+1]
+
+reader = easyocr.Reader(['en'])
+result = reader.readtext(cropped_image)
+print(result)
+
+cv2.imshow('test', edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
